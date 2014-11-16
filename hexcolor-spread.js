61749e457c8af27a7a1d09b81f1bcec0e539c9ce
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2014-11-16 23:38:38 +0100

    fix: don’t import the whole `hexcolor` module

--- hexcolor-spread.js
+++ hexcolor-spread.js
@@ -1,17 +1,17 @@
-import * as hexcolor from 'hexcolor';
+import { rgb, rgba, float3, float4 } from 'hexcolor';
 
-hexcolor.rgb.spread = function (vec) {
-  return hexcolor.rgba(vec[0], vec[1], vec[2], 255);
+rgb.spread = function (vec) {
+  return rgba(vec[0], vec[1], vec[2], 255);
 };
 
-hexcolor.rgba.spread = function (vec) {
-  return hexcolor.rgba(vec[0], vec[1], vec[2], vec[3]);
+rgba.spread = function (vec) {
+  return rgba(vec[0], vec[1], vec[2], vec[3]);
 };
 
-hexcolor.float3.spread = function (vec) {
-  return hexcolor.rgba(vec[0] * 255, vec[1] * 255, vec[2] * 255, 255);
+float3.spread = function (vec) {
+  return rgba(vec[0] * 255, vec[1] * 255, vec[2] * 255, 255);
 };
 
-hexcolor.float4.spread = function (vec) {
-  return hexcolor.rgba(vec[0] * 255, vec[1] * 255, vec[2] * 255, vec[3] * 255);
+float4.spread = function (vec) {
+  return rgba(vec[0] * 255, vec[1] * 255, vec[2] * 255, vec[3] * 255);
 };
